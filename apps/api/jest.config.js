d23dfe1dcc8ca916e2433c4952deed087ef20e7b
mrblackoutz <107147086+mrblackoutz@users.noreply.github.com>
2025-10-03 22:22:47 +0000

    feat: adicionar configuração do Jest e atualizar scripts de teste no package.json

---
+++ apps/api/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.module.ts',
+    '!**/*.interface.ts',
+    '!**/main.ts',
+    '!**/index.ts',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+};
